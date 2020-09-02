8df7d6522fa6deb7400ed2f775d9c8c777157fdd
Ahmed Ayachi <AhmedAyachi@users.noreply.github.com>
2020-09-02 19:28:19 +0100

    A

--- src/Components/MovieList/MovieList.js
+++ src/Components/MovieList/MovieList.js
@@ -9,6 +9,7 @@ export default function MovieList(){
     return (
         <div className={css.movielist}>
             {movies.map((movie,index)=><MovieCard key={`${movie.name}_${index}`} movie={movie}/>)}
+            {movies.length===0&&<h1 className={css.nomoviemsg}>Can't find movie</h1>}
         </div>
     )
 }
